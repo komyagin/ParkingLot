cb1ee6862340d833460bdee2e34aacdff227fead
Semyon Komyagin <komyagin.s@gmail.com>
2020-03-16 23:56:47 +0300

    Refactored

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -4,11 +4,6 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    var exit = false
-    while (!exit) {
-        print("What")
-    }
-
     val string = scanner.nextLine().split(" ")
     if (string[0] == "park") {
         println(string[2] + " car parked on the spot 2.")
@@ -19,17 +14,5 @@ fun main() {
             println("There is no car in the spot 2.")
         }
     }
-
-}
-
-fun check(array: IntArray): Boolean {
-    return true;
-}
-
-fun park(lot: Int) {
-
-}
-
-fun leave(lot: Int) {
 
 }
